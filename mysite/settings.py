c24ebcce86127af190219d79cc817264b0130588
nicitian <sa68707883.@163.com>
2018-05-14 22:56:19 +0800

    add host to ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')9_1wp*el(k1k41hxps_x8f+!f=zqn1^0jyl-v@6wngv&hag)1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['47.104.224.45']
 
 
 # Application definition
