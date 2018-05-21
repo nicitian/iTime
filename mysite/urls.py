35bebf164ebd82880e7f375ba12bd2e19da5934f
nicitian <sa68707883.@163.com>
2018-05-21 12:08:37 +0800

    alter url for static

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
+from django.conf.urls import url,include,static
 from django.contrib import admin
 from login import views
+import settings
+urlpatterns = []
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+
 urlpatterns = [
     url(r'^polls/',include('polls.urls',namespace='polls')),
     url(r'^admin/', admin.site.urls),
