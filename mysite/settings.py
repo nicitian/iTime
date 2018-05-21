49447ff7a82cf70c2efd80ca2bd03e40bfb02352
nicitian <sa68707883.@163.com>
2018-05-21 12:20:35 +0800

    static

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,9 +132,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
-if DEBUG:
 
-    STATICFILES_DIRS = (
+STATICFILES_DIRS = (
         os.path.join(BASE_DIR, "static"),
 
     )
